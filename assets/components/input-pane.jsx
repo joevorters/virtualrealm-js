74c68ff7c3f87d25b5bad6c833012515ebe81a83
Raymond Pulver IV <raymond.pulver_iv@uconn.edu>
2016-06-18 11:30:00 -0400

    update

---
+++ assets/components/input-pane.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class InputPane extends React.Component {
+  render() {
+    return (
+      <div className="panel-footer">
+        <input type="text" class="form-control" id="inputPane" />
+      </div>
+    )
+  }
+}
---
+++ assets/components/output-pane.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class OutputPane extends React.Component {
+  render() {
+    return (
+      <div className="panel-body">
+        Woop
+      </div>
+    )
+  }
+}
