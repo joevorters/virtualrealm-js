bc4e07bc62d3422cb18c22ccc399b3b73bcbd2b4
Raymond Pulver <raymond.pulver_iv@uconn.edu>
2016-06-18 15:13:56 -0400

    update

--- assets/components/application.jsx
+++ assets/components/application.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import OutputPane from './output-pane.jsx';
+import Console from './console.jsx';
 import NavBar from './navbar.jsx';
-import InputPane from './input-pane.jsx';
 
 export default class Application extends React.Component {
   render() {
     return (
-      <div className="panel panel-primary">
+      <div className="panel panel-primary app-container">
         <NavBar />
-        <OutputPane />
-        <InputPane />
+        <Console />
       </div>
     );
   }
